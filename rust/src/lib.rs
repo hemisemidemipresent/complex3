81a9550c48db7e895f9387d1c92fe36fd9555547
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-09-04 22:33:59 +0800

    used inferno color scheme for colored re-im and im-re plots, stretched out sigmoid function

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -16,9 +16,26 @@ pub use crate::rpneval::MathContext;
 
 pub use num_complex::Complex32;
 pub use std::f32::consts::{E, PI};
-pub use std::f32::{INFINITY, NEG_INFINITY};
 pub use std::vec::Vec;
 pub use wasm_bindgen::prelude::*;
+
+// ports console.log
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    // The `console.log` is quite polymorphic, so we can bind it with multiple
+    // signatures. Note that we need to use `js_name` to ensure we always call
+    // `log` in JS.
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+
+    // Multiple arguments too!
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
+}
 
 #[wasm_bindgen]
 pub fn evaluate(input: &str, n: i16, graph_type: u8, log_height: bool) -> Vec<f32> {
@@ -37,6 +54,7 @@ pub fn evaluate(input: &str, n: i16, graph_type: u8, log_height: bool) -> Vec<f3
             let result = ctx.eval(&expr, Complex32::new(re, im)).unwrap();
             // settles height
             match graph_type {
+                // Re-Im, height = real component
                 0 | 1 => {
                     let height = remove_inf(result.re);
                     if log_height {
@@ -51,6 +69,7 @@ pub fn evaluate(input: &str, n: i16, graph_type: u8, log_height: bool) -> Vec<f3
                         pos.push(height);
                     }
                 }
+                // Im-Re, height = imaginary component
                 2 | 3 => {
                     let height = remove_inf(result.im);
                     if log_height {
@@ -65,6 +84,7 @@ pub fn evaluate(input: &str, n: i16, graph_type: u8, log_height: bool) -> Vec<f3
                         pos.push(height);
                     }
                 }
+                // Mod-Arg, height = modulus
                 _ => {
                     let height = remove_inf(result.norm());
                     if log_height {
@@ -76,26 +96,32 @@ pub fn evaluate(input: &str, n: i16, graph_type: u8, log_height: bool) -> Vec<f3
             }
             // settles color
             match graph_type {
+                // Re-Im, color
                 0 => {
-                    let mut color_vec = hsvcolor(sig(result.im) * 2. * PI, saturation, 1.);
+                    //log(format!("{:?}", result.im).as_str());
+                    let mut color_vec = gradient(result.im);
                     color.append(&mut color_vec);
                 }
+                // Re-Im, BW
                 1 => {
                     let sig_im = sig(result.im);
                     color.push(sig_im);
                     color.push(sig_im);
                     color.push(sig_im);
                 }
+                // Im-Re, color
                 2 => {
-                    let mut color_vec = hsvcolor(sig(result.re) * 2. * PI, saturation, 1.);
+                    let mut color_vec = gradient(result.re);
                     color.append(&mut color_vec);
                 }
+                // Im-Re, BW
                 3 => {
                     let sig_re = sig(result.re);
                     color.push(sig_re);
                     color.push(sig_re);
                     color.push(sig_re);
                 }
+                // Mod-Arg
                 _ => {
                     let mut color_vec = hsvcolor(result.arg(), saturation, 1.);
                     color.append(&mut color_vec);
@@ -108,13 +134,16 @@ pub fn evaluate(input: &str, n: i16, graph_type: u8, log_height: bool) -> Vec<f3
 }
 
 fn remove_inf(val: f32) -> f32 {
-    match val {
-        f32::NAN => return 1E+38,
-        INFINITY => return 1E+38,
-        NEG_INFINITY => return -1E+38,
-        _ => return val,
+    if val.is_nan() {
+        return 1E+38; // handles NaN
+    } else if val.is_finite() {
+        return val; // handles normal values
+    } else if val.is_sign_positive() {
+        return 1E+38; // handles positive infinity
     }
+    return -1E+38; // handles negative infinity
 }
+
 // accepts h in radians
 fn hsvcolor(mut h: f32, s: f32, v: f32) -> Vec<f32> {
     // neg check
@@ -178,6 +207,16 @@ fn hsvcolor(mut h: f32, s: f32, v: f32) -> Vec<f32> {
     return vec;
 }
 
+fn gradient(y: f32) -> Vec<f32> {
+    let gradient = colorous::INFERNO;
+    let color = gradient.eval_continuous(sig(y) as f64);
+    let mut vec = Vec::new();
+    vec.push((color.r as f32) / 256.);
+    vec.push((color.g as f32) / 256.);
+    vec.push((color.b as f32) / 256.);
+    return vec;
+}
+
 fn sig(x: f32) -> f32 {
-    return 1. / (1. + E.powf(-x));
+    return 1. / (1. + E.powf(-x / 5.));
 }
